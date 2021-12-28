ebc0a13716a2ab369fa1e61ef6e3879bdf2dba9c
mibrahim234 <mohamm1417@gmail.com>
2021-12-27 19:08:18 -0500

    files

---
+++ Develop/public/js/service-worker.js
@@ -0,0 +1,11 @@
+const FILES_TO_CACHE = [
+    '/',
+    '/index.html',
+    '/manifest.webmanifest',
+    '/styles.css',
+    '/index.js',
+    '/db.js',
+    '/icons/icon-192x192.png',
+    '/icons/icon-512x512.png',
+    'https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css',
+  ];
